054785825bea6ad0b492c65d95cf93d1837bd398
mudjaycker <tombutoyi@gmail.com>
2024-01-13 11:11:08 +0200

    is_world_found bool found

--- svelte_ui/src/store.js
+++ svelte_ui/src/store.js
@@ -3,3 +3,4 @@ import { writable } from "svelte/store";
 export let meanings = writable([])
 export let synonyms = writable([])
 export let antonyms = writable([])
+export let is_word_found = writable(false)
