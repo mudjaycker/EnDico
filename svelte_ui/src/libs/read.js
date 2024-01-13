2c281981f3d27fb0679b363910b01c3410fd60d7
mudjaycker <tombutoyi@gmail.com>
2024-01-13 11:16:01 +0200

    set data in store only if it's found

--- svelte_ui/src/libs/read.js
+++ svelte_ui/src/libs/read.js
@@ -3,32 +3,36 @@ import {
   meanings as meanings_store,
   synonyms,
   antonyms,
+  is_word_found,
 } from "../store";
 
 export class Read {
   constructor(str) {
     this.str = str.toUpperCase();
   }
-  search() {
+  async search() {
+    is_word_found.set(false);
     let error_msg = "This word does not exist in the dictionary";
     try {
-      let data = getFromMerged(this.str);
-      let meanings = data["MEANINGS"];
+      let data = await getFromMerged(this.str);
       let meanings_list = [];
-
+      
       if (!data) {
         alert(error_msg);
       } else {
+        synonyms.set(data["SYNONYMS"]);
+        antonyms.set(data["ANTONYMS"]);
+        let meanings = data["MEANINGS"];
+        is_word_found.set(true);
         for (const key in meanings) {
           meanings_list.push(meanings[key]);
         }
         meanings_store.set(meanings_list);
-    }
-    
-    synonyms.set(data["SYNONYMS"]);
-    antonyms.set(data["ANTONYMS"]);
-    meanings_list = [];
+      }
+
+      meanings_list = [];
     } catch (e) {
+      is_word_found.set(false);
       alert(error_msg);
     }
   }
